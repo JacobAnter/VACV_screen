7df6230a95101681c7694d2c81e2ff1f4f5962b4
Jacob Anter <j.anter@hzdr.de>
2024-11-15 22:01:31 +0100

    Creating a Python script devoted to replicating the STRING file parsing functionality

---
+++ parsing_STRING_network.py
@@ -0,0 +1,30 @@
+"""
+The purpose of this Python script is to translate the functionality
+provided by the "parseStringNetwork.m" file from Matlab into Python. The
+aforementioned file is part of the source code made publicly available
+in the context of the publication "RNAi Screening Reveals Proteasome-
+and Cullin3-Dependent Stages in Vaccinia Virus Infection".
+"""
+
+import numpy as np
+
+def parse_STRING_network():
+    """
+    ...
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    string_network_arr: ndarray, shape=(n,n), dtype=...
+        ...
+    string_identifiers_arr: ndarray, shape=(n,), dtype=...
+        ...
+    string_gene_symbols_arr: ndarray, shape=(n,), dtype=...
+        ...
+    ENSP_arr: ndarray, shape=(n,), dtype=...
+        ...
+    """
+
+    return
