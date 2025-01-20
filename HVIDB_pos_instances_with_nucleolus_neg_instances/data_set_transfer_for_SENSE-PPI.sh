ef5b5f1b9d09a071c7ee79fc89fa503df481c1e0
Jacob Anter <j.anter@hzdr.de>
2025-01-20 10:34:19 +0100

    Creating a shell script for transferring the data set splits to the SENSE-PPI directories on Hemera

---
+++ HVIDB_pos_instances_with_nucleolus_neg_instances/data_set_transfer_for_SENSE-PPI.sh
@@ -0,0 +1,10 @@
+# The purpose of this BASH script is do automate the transfer of the
+# data set files to the SENSE-PPI directory on Hemera
+
+# As k-fold cross-validation with k being equal to 10 is supposed to be
+# performed, there are 10 different splits in total
+for i in $(seq 0 9)
+do
+    scp data_set_splits/{VACV_WR_pos_and_neg_PPIs_test_split_${i}.tsv,VACV_WR_pos_and_neg_PPIs_train_val_split_${i}.tsv} \
+    anter87@hemera5:~/PPI_prediction/SENSE-PPI/k-fold_split_${i}
+done
