57d203cf796902c67ca0231754cb58c7e4e98cf2
Jacob Anter <j.anter@hzdr.de>
2025-01-21 16:48:04 +0100

    Adjusting the shell script to the transfer of the chunks the test sets have been split into

--- HVIDB_pos_instances_with_nucleolus_neg_instances/data_set_transfer_for_SENSE-PPI.sh
+++ HVIDB_pos_instances_with_nucleolus_neg_instances/data_set_transfer_for_SENSE-PPI.sh
@@ -5,8 +5,10 @@
 
 # As k-fold cross-validation with k being equal to 10 is supposed to be
 # performed, there are 10 different splits in total
+# Additionally, there are three chunks for each split (in the case of
+# the test sets)
 for i in $(seq 0 9)
 do
-    scp data_set_splits/{VACV_WR_pos_and_neg_PPIs_test_split_${i}.tsv,VACV_WR_pos_and_neg_PPIs_train_val_split_${i}.tsv} \
+    scp data_set_splits/data_set_split_${i}/{VACV_WR_pos_and_neg_PPIs_test_set_split_${i}_chunk_0.tsv,VACV_WR_pos_and_neg_PPIs_test_set_split_${i}_chunk_1.tsv,VACV_WR_pos_and_neg_PPIs_test_set_split_${i}_chunk_2.tsv,VACV_WR_pos_and_neg_PPIs_test_set_prot_seqs_split_${i}_chunk_0.fasta,VACV_WR_pos_and_neg_PPIs_test_set_prot_seqs_split_${i}_chunk_1.fasta,VACV_WR_pos_and_neg_PPIs_test_set_prot_seqs_split_${i}_chunk_2.fasta} \
     anter87@hemera5:~/PPI_prediction/SENSE-PPI/k-fold_split_${i}
 done
