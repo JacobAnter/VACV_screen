39204d46a75b57fdb6efdc0feb352c22fdec4707
Jacob Anter <j.anter@hzdr.de>
2025-01-23 11:12:23 +0100

    Introduce sshpass into the shell script in order to obviate the necessity of repeatedly entering the password

--- HVIDB_pos_instances_with_nucleolus_neg_instances/data_set_transfer_for_SENSE-PPI.sh
+++ HVIDB_pos_instances_with_nucleolus_neg_instances/data_set_transfer_for_SENSE-PPI.sh
@@ -3,12 +3,26 @@
 # The purpose of this BASH script is do automate the transfer of the
 # data set files to the SENSE-PPI directory on Hemera
 
+# Usage of sshpass obviates the necessity to repeatedly enter the
+# password
+prompt="Enter password: "
+read -p "${prompt}" -s password
+echo "Password successfully entered."
+
 # As k-fold cross-validation with k being equal to 10 is supposed to be
 # performed, there are 10 different splits in total
 # Additionally, there are three chunks for each split (in the case of
 # the test sets)
+# Iterate over the 10 splits
 for i in $(seq 0 9)
 do
-    scp data_set_splits/data_set_split_${i}/{VACV_WR_pos_and_neg_PPIs_test_set_split_${i}_chunk_0.tsv,VACV_WR_pos_and_neg_PPIs_test_set_split_${i}_chunk_1.tsv,VACV_WR_pos_and_neg_PPIs_test_set_split_${i}_chunk_2.tsv,VACV_WR_pos_and_neg_PPIs_test_set_prot_seqs_split_${i}_chunk_0.fasta,VACV_WR_pos_and_neg_PPIs_test_set_prot_seqs_split_${i}_chunk_1.fasta,VACV_WR_pos_and_neg_PPIs_test_set_prot_seqs_split_${i}_chunk_2.fasta} \
-    anter87@hemera5:~/PPI_prediction/SENSE-PPI/k-fold_split_${i}
+    # Iterate over the three different chunks each test set has been
+    # subdivided into
+    for j in $(seq 0 3)
+    do
+        sshpass -p "${password}" scp data_set_splits/data_set_split_${i}/{VACV_WR_pos_and_neg_PPIs_test_set_split_${i}_chunk_${j}.tsv,VACV_WR_pos_and_neg_PPIs_test_set_prot_seqs_split_${i}_chunk_${j}.fasta} \
+        anter87@hemera5:~/PPI_prediction/SENSE-PPI/k-fold_split_${i}
+    done
 done
+
+echo "Data transfer accomplished."
