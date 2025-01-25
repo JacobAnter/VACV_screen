cfbe299eca1fcff43f0c7e652149bc95b608c656
Jacob Anter <j.anter@hzdr.de>
2025-01-25 14:44:41 +0100

    Creating a shell script performing results files transfer for xCAPT5 with XGBoost in an automated manner

---
+++ HVIDB_pos_instances_with_nucleolus_neg_instances/Results/Results_xCAPT5/results_files_transfer_with_XGBoost.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Usage of sshpass obviates the necessity to repeatedly enter the
+# password
+prompt="Enter password: "
+read -p "${prompt}" -s password
+echo "Password successfully entered."
+
+# Iterate over the 10 test sets
+for i in $(seq 0 9)
+do
+    # The xCAPT5 source code has been tweaked such that one output file
+    # is generated
+    # Thus, for each test set, only one file has to be transferred
+    sshpass -p "${password}" scp anter87@hemera5:~/PPI_prediction/xCAPT5/10-fold_cross-validation/without_training/with_XGBoost/xCAPT5_interaction_probs_VACV_WR_10-fold_cross-val_test_set_${i}_with_XGBoost.tsv \
+    Results_with_XGBoost
+done
+
+echo "Data transfer accomplished."
