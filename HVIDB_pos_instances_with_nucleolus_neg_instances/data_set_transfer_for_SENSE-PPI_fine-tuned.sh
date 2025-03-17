d12aa9fc6b23797d36b944f57279af6b6561a7c1
Jacob Anter <j.anter@hzdr.de>
2025-03-17 11:32:54 +0100

    Creating a shell script automating data set transfer for fine-tuned SENSE-PPI model

---
+++ HVIDB_pos_instances_with_nucleolus_neg_instances/data_set_transfer_for_SENSE-PPI_fine-tuned.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# The purpose of this BASH script is to automate the transfer of the
+# data set files to the directory of the fine-tuned SENSE-PPI model on
+# Hemera
+
+# Usage of sshpass obviates the necessity to repeatedly enter the
+# password
+prompt="Enter password: "
+read -p "${prompt}" -s password
+echo "Password successfully entered."
+
+# As k-fold cross-validation with k being equal to 10 is supposed to be
+# performed, there are 10 different splits in total
+# Additionally, there are four chunks for each split (in the case of the
+# test sets)
+# Iterate over the 10 splits
+for i in $(seq 0 9)
+do
+    # Iterate over the four different chunks each test set has been
+    # subdivided into
+    for j in $(seq 0 3)
+    do
+        sshpass -p "${password}" scp data_set_splits/data_set_split_${i}/{VACV_WR_pos_and_neg_PPIs_test_set_split_${i}_chunk_${j}.tsv,VACV_WR_pos_and_neg_PPIs_test_set_prot_seqs_split_${i}_chunk_${j}.fasta} \
+        anter87@hemera5:~/PPI_prediction/SENSE-PPI/10-fold_cross-val_on_VACV_WR_pos_and_neg_data_set_fine-tuned/k-fold_split_${i}
+    done
+done
+
+echo "Data transfer accomplished."
