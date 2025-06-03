e809ae3d34b16388922900397a3ff5ac5545f873
Jacob Anter <j.anter@hzdr.de>
2025-06-03 17:52:13 +0200

    Creating a Python script for the generation of a multiprocessing draft

---
+++ SENSE-PPI/ESM-3_implementation/multiprocessing_test.py
@@ -0,0 +1,68 @@
+"""
+The purpose of this Python script is to generate a draft of
+multiprocessing for the embedding of protein sequences via ESM-3.
+"""
+
+import torch
+from esm.models.esmc import ESMC
+from esm.sdk.api import ESMProtein, LogitsConfig
+from biotite.sequence.io import fasta
+import multiprocessing as mp
+
+MAX_LEN = 800
+
+# As a preliminary step, extract the pairs of UniProt IDs and sequences
+# from the FASTA file
+path_to_fasta = (
+    "/Users/jacobanter/Documents/Code/VACV_screen/SENSE-PPI/ESM-3_"
+    "implementation/sample_fasta_file.fasta"
+)
+sample_fasta = fasta.FastaFile.read(path_to_fasta)
+id_seq_pairs = list(sample_fasta.items())
+
+# Now, define a worker function that will be executed in parallel
+def embed_sequence(id_seq_pair):
+    uniprot_ac = id_seq_pair[0]
+    seq = id_seq_pair[1]
+
+    model = ESMC.from_pretrained("esmc_300m").to("cpu").eval()
+
+    # Convert the string to an ESMProtein
+    protein = ESMProtein(sequence=seq)
+
+    # Finally, encode the protein and its embeddings
+    with torch.no_grad():
+        token_tensor = model.encode(protein)
+
+        output = model.logits(
+            token_tensor,
+            LogitsConfig(sequence=False, return_embeddings=True)
+        )
+
+        # Bear in mind that during tokenisation, a beginning-of-sequence
+        # token as well as an end-of-sequence token are added to the
+        # beginning and the end of the sequence, respectively
+        # Thus, the embedding tensor has to be sliced such that they are
+        # removed
+        embedding = output.embeddings[0, 1:-1]
+
+        # As a last step, the embedding is saved to a file
+        result = {"label": uniprot_ac}
+        # Call clone on tensors to ensure tensors are not views into a
+        # larger representation
+        # See https://github.com/pytorch/pytorch/issues/1995
+        result["representations"] = {
+            36: embedding[:MAX_LEN].clone()
+        }
+        torch.save(
+            result,
+            "/Users/jacobanter/Documents/Code/VACV_screen/SENSE-PPI/"
+            f"ESM-3_implementation/{uniprot_ac}.pt"
+        )
+
+
+if __name__ == "__main__":
+    n_workers = mp.cpu_count()
+
+    with mp.Pool(processes=n_workers) as pool:
+        pool.map(embed_sequence, id_seq_pairs)
